7f114764a46cb248ed4742ba2951bb35af74bbbc
shreegowri <gaganandh@gmail.com>
2022-08-04 11:34:23 +0530

    [shreegowri] validating for the phone number

--- js/form.js
+++ js/form.js
@@ -29,6 +29,33 @@
                 error_email.textContent="email is invalid"
             }
         });
+        //uc3
+        const phoneNumber=document.querySelector('#Telephone')
+        const phoneNumber_error=document.querySelector('.error-telephone')
+        phoneNumber.addEventListener('input',function()
+        {
+            let phoneNumberRegex=RegExp(("^[0-9]{2}\\s[0-9]{10}$"));
+            if(phoneNumberRegex.test(phoneNumber.value))
+            {
+                phoneNumber_error.innerHTML="";
+            }
+            else{
+                phoneNumber_error.innerHTML="phoneNumber is invalid"
+            }
+        });
+        const teleNumber=document.querySelector('#Number')
+        const teleNumber_error=document.querySelector('.error-number')
+        teleNumber.addEventListener('input',function()
+        {
+            let teleNumberRegex=RegExp(("^[0-9]{10}$"));
+            if(teleNumberRegex.test(teleNumber.value))
+            {
+                teleNumber_error.innerHTML="";
+            }
+            else{
+                teleNumber_error.innerHTML="telephoneNumber is invalid"
+            }
+        });
         
     
         
